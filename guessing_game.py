799b52222a5f076d7df94d7f37ca774f75adfee8
Roman <cypherpanda@pm.me>
2020-02-20 20:12:24 +0100

    updated cleaner code - learning debugging

--- guessing_game.py
+++ guessing_game.py
@@ -3,19 +3,35 @@ answer = 5
 print("Please guess number between 1 and 10: ")
 guess = int(input())
 
-if guess < answer:
-    print("You need more.")
-    guess = int(input())
-    if guess == answer:
-        print("Well done!")
-    else:
-        print("Sorry, wrong.")
-elif guess > answer :
-    print("You need less")
-    guess = int(input())
-    if guess == answer:
-        print("Well done!")
-    else:
-        print("Sorry, wrong.")
+if guess != answer:
+     if guess < answer:
+         print("Please guess higher")
+     else:
+         print("Please guess lower.")
+     guess = int(input())
+     if guess == answer:
+         print("Well done!")
+     else:
+         print("Sorry, wrong. :(")
 else:
-    print("Good job!")
+    print("WOW, good job!")
+       
+
+# OLD CODE
+# if guess < answer:
+#     print("You need more.")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done!")
+#     else:
+#         print("Sorry, wrong.")
+# elif guess > answer :
+#     print("You need less")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done!")
+#     else:
+#         print("Sorry, wrong.")
+# else:
+#     print("Good job!")
+
