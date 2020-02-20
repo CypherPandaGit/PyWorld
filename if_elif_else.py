793db1c6f80336c8e2909ae9c39037c0837e868d
Roman <cypherpanda@pm.me>
2020-02-20 13:11:28 +0100

    learning if, elif

--- if_elif_else.py
+++ if_elif_else.py
@@ -4,7 +4,7 @@ print("You are {0} years old and".format(age))
 
 if age <18:
     print("you can't vote {0}. Come back in {1} years.".format(name, 18 - age))
+elif age >=120:
+    print("eeeh, something is wrong, {0}?".format(name))
 elif age >= 18:
     print("you can vote. Great!")
-else:
-    print("something is wrong, {0}.".format(name))
