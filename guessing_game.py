1fc1811a5775e7d4f8e277b4c05f3f029f3738a6
Roman <cypherpanda@pm.me>
2020-02-20 19:44:54 +0100

    learning debugging with if, elif, else statements

--- guessing_game.py
+++ guessing_game.py
@@ -5,7 +5,17 @@ guess = int(input())
 
 if guess < answer:
     print("You need more.")
+    guess = int(input())
+    if guess == answer:
+        print("Well done!")
+    else:
+        print("Sorry, wrong.")
 elif guess > answer :
     print("You need less")
+    guess = int(input())
+    if guess == answer:
+        print("Well done!")
+    else:
+        print("Sorry, wrong.")
 else:
     print("Good job!")
