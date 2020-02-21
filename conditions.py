a81231fd5ab804efeb558bc9856608391c7e4bcb
CypherPandaGit <56232713+cypherpandagit@users.noreply.github.com>
2020-02-21 12:57:49 +0100

    conditions AND, OR

---
+++ conditions.py
@@ -0,0 +1,8 @@
+age = int(input("How old are you? "))
+
+# cleaner code version
+if 18 <= age <= 65:
+    print("Work harder!")
+else:
+    print("Great, you don't need to work. :)")
+print("*" * 20)
