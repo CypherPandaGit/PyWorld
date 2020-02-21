bc8c6b6448c4a381fe5b9fb1d516fb72b913828a
Roman <cypherpanda@pm.me>
2020-02-21 13:39:24 +0100

    True, False

--- truefalse.py
+++ truefalse.py
@@ -1,8 +1,21 @@
-day = "Monday"
-temperature = 30
-raining = True
+# Old code
+# day = "Friday"
+# temperature = 30
+# raining = False
+#
+# if (day == "Saturday" and temperature > 27) and not raining:
+#     print("Go swimming")
+# else:
+#     print("Learn Python")
 
-if day == "Saturday" and temperature > 27 and not raining:
-    print("Go swimming")
+# if 0:
+#     print("True")
+# else:
+#     print("False")
+
+name = input("Please enter your name: ")
+# if name:
+if name != "" and name != " ":
+    print("Hello, {0}".format(name))
 else:
-    print("Stay home and learn Python")
+    print("You are the man with no name?")
