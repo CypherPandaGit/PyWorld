997913a0ba73348db896f035838b2a91ea3fd215
CypherPandaGit <56232713+cypherpandagit@users.noreply.github.com>
2020-02-24 17:35:48 +0100

    added .casefold()

--- checkingin.py
+++ checkingin.py
@@ -2,7 +2,7 @@ words = "Hauptstadt Wien"
 
 letter = input("Enter a character: ")
 
-if letter in words:
+if letter.casefold() in words.casefold():
     print("Yes, '{0}' is in '{1}'.".format(letter, words))
 else:
     print("No, '{0}' is not in '{1}'.".format(letter, words))
