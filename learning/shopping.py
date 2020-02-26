9ec6f53771002810c4c5f6f51ee68509bdb72ba5
CypherPandaGit <56232713+cypherpandagit@users.noreply.github.com>
2020-02-26 14:39:53 +0100

    no message

--- learning/shopping.py
+++ learning/shopping.py
@@ -1,1 +1,26 @@
 shopping_list = ["milk", "pasta", "egg", "spam", "bread", "rice"]
+
+# for item in shopping_list:
+#     if item != "spam":
+#         print("Buy " + item)
+
+# for item in shopping_list:
+#     if item == "spam":
+#         continue
+#     print("Buy " + item)
+
+item_to_find = "spam"
+found_at = None
+
+# for index in range(len(shopping_list)):
+#     if shopping_list[index] == item_to_find:
+#         found_at = index
+#         break
+
+if item_to_find in shopping_list:
+    found_at = shopping_list.index(item_to_find)
+
+if found_at is not None:
+    print("Item found at position {}".format(found_at + 1))
+else:
+    print("{} not found".format(item_to_find))
