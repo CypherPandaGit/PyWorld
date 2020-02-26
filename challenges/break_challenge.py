546dc8f207c752c5e458aa7c74386dc2b738f14e
CypherPandaGit <56232713+cypherpandagit@users.noreply.github.com>
2020-02-26 15:55:32 +0100

    Just edited little things

---
+++ challenges/break_challenge.py
@@ -0,0 +1,14 @@
+# for i in range(0, 100, 7):
+#     print(i)
+#     if i > 0 and i % 11 == 0:
+#         break
+
+for i in range(0, 21):
+    if i > 0 and i % 3 and i % 5:
+        print(i)
+        continue
+
+# for i in range(21):
+#     if i % 3 == 0 or i % 5 == 0:
+#         continue
+#     print(i)
