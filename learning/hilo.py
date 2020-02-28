65eec707e4737b5ceb834ffdccc73a75fafaf74f
CypherPandaGit <56232713+cypherpandagit@users.noreply.github.com>
2020-02-28 20:21:29 +0100

    Binary search

--- learning/hilo.py
+++ learning/hilo.py
@@ -6,19 +6,24 @@ input("Press ENTER key to start")
 
 guesses = 1
 while True:
-    guesses = low + (high - low) // 2
+    guess = low + (high - low) // 2
     high_low = input("My guess is {}. Should i guess higher or lower? "
-                     "Enter h or l, or c if my guess was correct"
-                     .format(guesses)).casefold()
+                     "Enter h or l, or c if my guess was correct "
+                     .format(guess)).casefold()
 
     if high_low == "h":
         # Guess higher. The low end of the range becomes 1 greater than the guess.
+        low = guess + 1
 
     elif high_low == "l":
         # Guess lower. The high end of the range becomes one less than the guess.
+        high = guess - 1
 
     elif high_low == "c":
         print("I got it in {} guesses!".format(guesses))
+        break
 
     else:
         print("Please enter h, l or c")
+
+    guesses = guesses + 1
