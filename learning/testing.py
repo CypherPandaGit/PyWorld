d1dd423ee261edd4903af416c43b11cea9d3969f
CypherPandaGit <56232713+cypherpandagit@users.noreply.github.com>
2020-03-03 18:24:14 +0100

    update

--- learning/testing.py
+++ learning/testing.py
@@ -1,7 +1,7 @@
-import math
+def div42by(divide_by):
+    return 42 / divide_by
 
-def logarithm(x):
-    return math.log(x, 2)
-
-print(logarithm(10))
-print(logarithm(33))
+print(div42by(2))
+print(div42by(12))
+print(div42by(0))
+print(div42by(1))
