777ab1e8641a31385942d86c08ab1c81e1d77a41
CypherPandaGit <56232713+cypherpandagit@users.noreply.github.com>
2020-03-03 19:42:47 +0100

    try and except, funcions

---
+++ learning/try_except.py
@@ -0,0 +1,15 @@
+def cats():
+    print("How many cats do you have?")
+    cats = input()
+
+    try:
+        if int(cats) >= 4:
+            print("That's a lot of cats.")
+        else:
+            print("That is not that many cats.")
+    except ValueError:
+        print("You did not enter a number.")
+
+print("This will be executed first.")
+print("And cats function second.")
+cats()
