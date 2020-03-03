32bf05f55a218b2c6ae476386ee05a349f441f35
CypherPandaGit <56232713+cypherpandagit@users.noreply.github.com>
2020-03-03 19:38:02 +0100

    update

--- learning/testing.py
+++ learning/testing.py
@@ -1,10 +1,10 @@
-def div42by(divide_by):
-    try:
-        return 42 / divide_by
-    except ZeroDivisionError:
-        print("Error: You tried to divide by zero.")
+print("How many cats do you have?")
+cats = input()
 
-print(div42by(2))
-print(div42by(12))
-print(div42by(0))
-print(div42by(1))
+try:
+    if int(cats) >= 4:
+        print("That's a lot of cats.")
+    else:
+        print("That is not that many cats.")
+except ValueError:
+    print("You did not enter a number.")
