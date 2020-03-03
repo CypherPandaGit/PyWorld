2112ff151d73a8f69cfc01aa62e786f13217ec1d
CypherPandaGit <56232713+cypherpandagit@users.noreply.github.com>
2020-03-03 19:19:04 +0100

    update

--- learning/testing.py
+++ learning/testing.py
@@ -1,5 +1,8 @@
 def div42by(divide_by):
-    return 42 / divide_by
+    try:
+        return 42 / divide_by
+    except ZeroDivisionError:
+        print("Error: You tried to divide by zero.")
 
 print(div42by(2))
 print(div42by(12))
