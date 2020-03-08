c925f85097c1c09f1855c99ce2bddfb3deeb49d1
CypherPandaGit <56232713+cypherpandagit@users.noreply.github.com>
2020-03-08 12:45:21 +0100

    learning lists

--- learning/lists.py
+++ learning/lists.py
@@ -1,6 +1,7 @@
 # ip_address = input("Please input an IP address: ")
 # print(ip_address.count("."))
 
+
 parrot_list = ["non pinin", "no more", "a stiff", "bereft of live"]
 for state in parrot_list:
     print("This parrot is " + state)
