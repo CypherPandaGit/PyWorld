8ecfdf4e96f69a3582a04ab672b9c2d7ab8d070b
CypherPandaGit <56232713+cypherpandagit@users.noreply.github.com>
2020-03-09 09:32:06 +0100

    learning lists

--- learning/lists.py
+++ learning/lists.py
@@ -3,5 +3,9 @@
 
 
 parrot_list = ["non pinin", "no more", "a stiff", "bereft of live"]
+
+print("This parrot is ...")
+print("What you want to add? ")
+parrot_list.append(input())
 for state in parrot_list:
     print("This parrot is " + state)
