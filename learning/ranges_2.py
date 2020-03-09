f84b9f23f047c90bde3cfe60dd7d95eb4b48636b
CypherPandaGit <56232713+cypherpandagit@users.noreply.github.com>
2020-03-09 15:27:57 +0100

    ranges again

--- learning/ranges_2.py
+++ learning/ranges_2.py
@@ -7,21 +7,45 @@
 # print(even)
 # print(odd)
 
-my_string = "abcdefghijklmnopqrstuvwxyz"
-print(my_string.index('e'))
-print(my_string[4])
+#----------------------------------------
 
-small_decimals = range(0, 10)
-print(small_decimals)
+# my_string = "abcdefghijklmnopqrstuvwxyz"
+# print(my_string.index('e'))
+# print(my_string[4])
+#
+# small_decimals = range(0, 10)
+# print(small_decimals)
+#
+# print(small_decimals.index(3))
+#
+# odd = range(1, 10000, 2)
+# print(odd)
+#
+# print(odd[985])
+#
+# sevens = range(7, 100000, 7)
+# x = int(input("Please enter a positive number less than a one million: "))
+# if x in sevens:
+#     print("{} is divisible by 7".format(x))
 
-print(small_decimals.index(3))
+#-------------------------------------------
 
-odd = range(1, 10000, 2)
-print(odd)
+for i in range(99, 0, -2):
+    print(i)
 
-print(odd[985])
+print("-" * 30)
 
-sevens = range(7, 100000, 7)
-x = int(input("Please enter a positive number less than a one million: "))
-if x in sevens:
-    print("{} is divisible by 7".format(x))
+r = range(0, 100)
+for i in r[::-2]:
+    print(i)
+
+print("-" * 30)
+
+if (range(0, 100)[::-2] == range(99, 0, -2)):
+    print("True")
+    
+print("-" * 30)
+print("-" * 30)
+
+my_string_reversed = "hello how are you"
+print(my_string_reversed[::-1])
