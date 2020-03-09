4bd627f330a72afe2684f5d0d334be49b4d80bcf
CypherPandaGit <56232713+cypherpandagit@users.noreply.github.com>
2020-03-09 10:08:34 +0100

    learning lists

--- learning/lists.py
+++ learning/lists.py
@@ -11,13 +11,15 @@ print("What you want to add? ")
 for state in parrot_list:
     print("This parrot is " + state)
 
-print("-------------")
+print("---------------------------------")
+print("---------------------------------")
+
 even = [2, 4, 6, 8, 10]
 odd = [1, 3, 5, 7, 9]
-print(even)
-print(odd)
+print("Even numbers: {}".format(even))
+print("Odd numbers: {}".format(odd))
 numbers = even + odd
-print(numbers)
+print("Merged: {}".format(numbers))
 
 print("--------------")
 print("len()")
