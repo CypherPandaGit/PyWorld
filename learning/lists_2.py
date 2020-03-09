9a207f822d0a545a363d0651dfc6a31873d9de6a
CypherPandaGit <56232713+cypherpandagit@users.noreply.github.com>
2020-03-09 12:46:58 +0100

    learning lists

--- learning/lists_2.py
+++ learning/lists_2.py
@@ -9,8 +9,25 @@
 #
 # print(list("The lists are equal"))
 
-even = [2, 4, 6, 8]
+# -----------------------------------
+# even = [2, 4, 6, 8]
+#
+# another_even = even
+#
+# print(another_even is even)
+#
+# another_even.sort(reverse=True)
+# print(even)
 
-another_even = even
-another_even.sort(reverse=True)
-print(even)
+# ------------------------------------
+
+even = [2, 4, 6, 8]
+odd = [1, 3, 5, 7, 9]
+
+numbers = [even, odd]
+
+for number_set in numbers:
+    print(number_set)
+
+    for value in number_set:
+        print(value)
