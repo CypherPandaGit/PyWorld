522261f73871faf051b2dcabae10da4c560ddd3b
CypherPandaGit <56232713+cypherpandagit@users.noreply.github.com>
2020-03-09 09:39:35 +0100

    learning lists

--- learning/lists.py
+++ learning/lists.py
@@ -6,6 +6,24 @@ parrot_list = ["non pinin", "no more", "a stiff", "bereft of live"]
 
 print("This parrot is ...")
 print("What you want to add? ")
-parrot_list.append(input())
+# parrot_list.append(input())
+
 for state in parrot_list:
     print("This parrot is " + state)
+
+print("-------------")
+even = [2, 4, 6, 8, 10]
+odd = [1, 3, 5, 7, 9]
+print(even)
+print(odd)
+numbers = even + odd
+print(numbers)
+
+print("--------------")
+print("len()")
+print(len(numbers))
+
+print("--------------")
+print("numbers.sort")
+numbers.sort()
+print(numbers)
