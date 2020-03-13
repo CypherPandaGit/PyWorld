c54dc35ba94d6892d87005a63923af0e941e671c
CypherPandaGit <56232713+cypherpandagit@users.noreply.github.com>
2020-03-13 20:23:54 +0100

    slices

--- learning/testing.py
+++ learning/testing.py
@@ -10,7 +10,17 @@
 # print(spam)
 
 
-spam = [10, 20, 30]
-print(spam)
-spam[1:3] = ["CAT", "MOUSE", "DOG"]
-print(spam)
+# spam = [10, 20, 30]
+# print(spam)
+# spam[1:3] = ["CAT", "MOUSE", "DOG"]
+# print(spam)
+
+# for i in range(4):
+#     print(i)
+#
+# my_list = list(range(5))
+# print(my_list)
+
+supplies = ['Apfel', 'Birne', 'Erdbeere', 'Himbeere']
+for i in range(len(supplies)):
+    print('Position {0} in supplies is: {1}'.format(str(i + 1), supplies[i]))
