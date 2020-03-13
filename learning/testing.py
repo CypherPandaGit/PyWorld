f37b00dcd99d383827ae487bab12c73ffc80470a
CypherPandaGit <56232713+cypherpandagit@users.noreply.github.com>
2020-03-13 20:06:41 +0100

    slices

--- learning/testing.py
+++ learning/testing.py
@@ -1,10 +1,16 @@
-spam = "Hello"
-spam = [10, 20 , 30]
+# spam = "Hello"
+# spam = [10, 20 , 30]
+#
+# spam[1] = "Hello"
+#
+# print(spam)
+#
+# spam[1:3] = ['cat', 'dog', 'mouse']
+#
+# print(spam)
 
-spam[1] = "Hello"
 
+spam = [10, 20, 30]
 print(spam)
-
-spam[1:3] = ['cat', 'dog', 'mouse']
-
+spam[1:3] = ["CAT", "MOUSE", "DOG"]
 print(spam)
