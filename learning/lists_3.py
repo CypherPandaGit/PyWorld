90bee79b6733605937d2650205860c80b619d0d2
CypherPandaGit <56232713+cypherpandagit@users.noreply.github.com>
2020-03-15 13:38:06 +0100

    slices

--- learning/lists_3.py
+++ learning/lists_3.py
@@ -1,17 +1,26 @@
-menu = []
-menu.append(["egg", "spam", "bacon"])
-menu.append(["egg", "sausage", "bacon"])
-menu.append(["egg", "spam"])
-menu.append(["egg", "bacon", "spam"])
-menu.append(["egg", "bacon", "sausage", "spam"])
-menu.append(["spam", "bacon", "sausage", "spam"])
-menu.append(["spam", "egg", "spam", "spam", "bacon", "spam"])
-menu.append(["spam", "egg", "sausage", "spam"])
+import copy
 
-print(menu)
+# menu = []
+# menu.append(["egg", "spam", "bacon"])
+# menu.append(["egg", "sausage", "bacon"])
+# menu.append(["egg", "spam"])
+# menu.append(["egg", "bacon", "spam"])
+# menu.append(["egg", "bacon", "sausage", "spam"])
+# menu.append(["spam", "bacon", "sausage", "spam"])
+# menu.append(["spam", "egg", "spam", "spam", "bacon", "spam"])
+# menu.append(["spam", "egg", "sausage", "spam"])
+#
+# print(menu)
+#
+# for meal in menu:
+#     if not "spam" in meal:
+#         print(meal)
+#         for item in meal:
+#             print(item)
 
-for meal in menu:
-    if not "spam" in meal:
-        print(meal)
-        for item in meal:
-            print(item)
+spam = ['A', 'B', 'C', 'D'] # list
+# spam = cheese # this is just reference to spam list
+cheese = copy.deepcopy(spam) # copy spam list to cheese
+
+print(cheese)
+print(spam)
