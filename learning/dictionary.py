cf3e68dbc233eaae4f8357f6bb9650b35fe495d4
CypherPandaGit <56232713+cypherpandagit@users.noreply.github.com>
2020-03-15 14:12:24 +0100

    slices

--- learning/dictionary.py
+++ learning/dictionary.py
@@ -2,3 +2,11 @@ my_cat = {'size': 'fat', 'color': 'gray', 'disposition': 'loud'}
 print(my_cat['size'])
 
 print("My cat has {} fur.".format(my_cat['color']))
+
+keys = {1234: 'password combination', 4322: 'luggage combo'}
+print(keys.values())
+
+
+picnic_items = {'apples': 5, 'cups': 2, 'napkins': 10}
+print("I'm bringing {} to the picnic".format(str(picnic_items.get('napkins', 0))))
+
