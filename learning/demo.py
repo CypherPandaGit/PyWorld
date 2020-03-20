01f98248ccec2c20dc37e6ec9a810784b2bd60a9
CypherPandaGit <56232713+cypherpandagit@users.noreply.github.com>
2020-03-20 15:52:28 +0100

    demo tuples

--- learning/demo.py
+++ learning/demo.py
@@ -1,15 +1,23 @@
-welcome = "Welcome to my Nightmare", "Alice Cooper", 1975
-bad = "Bad Company", "Bad Company", 1974
-budgie = "Nightflight", "Budgie", 1981
-imelda = "More Mayhem", "Imelda May", 2011, (1, "Pulling the rug"), (2, "Psycho"), (3, "Mayhem"), (4, "Kentish Town Waltz")
+# welcome = "Welcome to my Nightmare", "Alice Cooper", 1975
+# bad = "Bad Company", "Bad Company", 1974
+# budgie = "Nightflight", "Budgie", 1981
+# imelda = "More Mayhem", "Imelda May", 2011, (1, "Pulling the rug"), (2, "Psycho"), (3, "Mayhem"), (4, "Kentish Town Waltz")
+#
+# print(imelda)
+#
+# title, artist, year, track1, track2, track3, track4 = imelda
+# print(title)
+# print(artist)
+# print(year)
+# print(track1)
+# print(track2)
+# print(track3)
+# print(track4)
 
-print(imelda)
 
-title, artist, year, track1, track2, track3, track4 = imelda
-print(title)
-print(artist)
-print(year)
-print(track1)
-print(track2)
-print(track3)
-print(track4)
+imelda = "More Mayhem", "Imelda May", 2011, (
+    (1, "Pulling the rug"), (2, "Psycho"), (3, "Mayhem"), (4, "Kentish Town Waltz"))
+
+# print("{}, {}, {}".format(imelda[0], imelda[1], imelda[2]))
+# print("{}, {}, {}".format(imelda[3][0], imelda[3][1], imelda[3][2]))
+
