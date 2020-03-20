cd0dd8e5160b65c266083182fbad4f28206f6577
CypherPandaGit <56232713+cypherpandagit@users.noreply.github.com>
2020-03-20 15:24:25 +0100

    demo tuples

--- learning/demo.py
+++ learning/demo.py
@@ -1,8 +1,15 @@
-a = b = c = d = 12
-print(c)
-a, b = 12, 13
-print(a, b)
+welcome = "Welcome to my Nightmare", "Alice Cooper", 1975
+bad = "Bad Company", "Bad Company", 1974
+budgie = "Nightflight", "Budgie", 1981
+imelda = "More Mayhem", "Imelda May", 2011, (1, "Pulling the rug"), (2, "Psycho"), (3, "Mayhem"), (4, "Kentish Town Waltz")
 
-a, b = b, a
-print("a is {}".format(a))
-print("b is {}".format(b))
+print(imelda)
+
+title, artist, year, track1, track2, track3, track4 = imelda
+print(title)
+print(artist)
+print(year)
+print(track1)
+print(track2)
+print(track3)
+print(track4)
