39b153d9a28e80ac9921a21203248dbb87955329
Roman <cypherpanda@pm.me>
2020-03-22 10:29:21 +0100

    dict

--- learning/dictionaries.py
+++ learning/dictionaries.py
@@ -4,7 +4,7 @@ fruit = {"orange": "a sweet, orange, citrus fruit",
          "grape": "a small, sweet fruit growing in bunches",
          "lime": "a sour, green citrus fruit"}
 
-# print(fruit)
+print(fruit)
 # print(fruit["lemon"])
 #
 # fruit["pear"] = "an odd shaped apple"
@@ -12,14 +12,20 @@ fruit = {"orange": "a sweet, orange, citrus fruit",
 #
 # print(fruit["pear"])
 
-print(fruit)
+# print(fruit)
+#
+# while True:
+#     dict_key = input("Please enter a fruit: ")
+#     if dict_key == "quit":
+#         break
+#     if dict_key in fruit:
+#         description = fruit.get(dict_key)
+#         print(description)
+#     else:
+#         print("We don't have a " + dict_key)
 
-while True:
-    dict_key = input("Please enter a fruit: ")
-    if dict_key == "quit":
-        break
-    if dict_key in fruit:
-        description = fruit.get(dict_key)
-        print(description)
-    else:
-        print("We don't have a " + dict_key)
+
+ordered_keys = list(fruit.keys())
+ordered_keys.sort()
+for f in ordered_keys:
+    print(f + " - " + fruit[f])
