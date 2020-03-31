f2ec956a39458b37522f6541152741bdaceaaec5
CypherPandaGit <56232713+cypherpandagit@users.noreply.github.com>
2020-03-31 15:24:18 +0200

    little edit

--- learning/main.py
+++ learning/main.py
@@ -16,22 +16,22 @@ print(challenge[:6] + challenge[6:])
 
 print(challenge[:])
 """
-
-"""
-num = "9,223;372:036 854,775;807"
-separators = num[1::4]
-print(separators)
-
-values = "".join(char if char not in separators else " " for char in num).split()
-print([int(val) for val in values])
-"""
-
-
-letters = "abcdefghijklmnopqrstuvwxyz"
-print(len(letters))
-print(letters)
-print(letters[:-9:-1])
-print(letters[-4:])
-print(letters[-1:])
-print(letters[:1])
-print(letters[0])
+#
+# """
+# num = "9,223;372:036 854,775;807"
+# separators = num[1::4]
+# print(separators)
+#
+# values = "".join(char if char not in separators else " " for char in num).split()
+# print([int(val) for val in values])
+# """
+#
+#
+# letters = "abcdefghijklmnopqrstuvwxyz"
+# print(len(letters))
+# print(letters)
+# print(letters[:-9:-1])
+# print(letters[-4:])
+# print(letters[-1:])
+# print(letters[:1])
+# print(letters[0])
