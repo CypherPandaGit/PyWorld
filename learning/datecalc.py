d142315f92f903dc25e36aae9f74a87c5ec43c22
Roman <cypherpanda@pm.me>
2020-04-12 13:48:32 +0200

    date and time

--- learning/datecalc.py
+++ learning/datecalc.py
@@ -1,10 +1,30 @@
+# import time
+#
+# print(time.gmtime(0))
+#
+# local_time = time.localtime()
+#
+# print(local_time)
+# print('Year: ', local_time[0], local_time.tm_year)
+# print('Month: ', local_time[1], local_time.tm_mon)
+# print('Day: ', local_time[2], local_time.tm_mday)
+
+#-----------------------------------------------------
+
 import time
+from time import time as my_timer
+import random
 
-print(time.gmtime(0))
+input('Press enter to start')
 
-local_time = time.localtime()
+wait_time = random.randint(1, 6)
+time.sleep(wait_time)
+start_time = my_timer()
+input('Press enter to stop')
 
-print(local_time)
-print('Year: ', local_time[0], local_time.tm_year)
-print('Month: ', local_time[1], local_time.tm_mon)
-print('Day: ', local_time[2], local_time.tm_mday)
+end_time = my_timer()
+
+print('Started at ' + time.strftime('%X', time.localtime(start_time)))
+print('Ended at ' + time.strftime('%X', time.localtime(end_time)))
+
+print(f"Your reaction time was {end_time - start_time} seconds.")
