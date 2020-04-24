1470839c74475ac1a7f213433e96b4d549df4f87
Roman <cypherpanda@pm.me>
2020-04-24 12:11:40 +0200

    first calculator with modules

--- learning/modules/app.py
+++ learning/modules/app.py
@@ -3,28 +3,39 @@ import calc as cc
 
 def user_input():
 
-    a = int(input())
-    b = int(input())
-    print('Use these functions: add, sub, multi, div\nor q for quit')
+    print('Enter your first number: ')
+    a = float(input())
+    print('And your second number: ')
+    b = float(input())
+    print('Now you can use these functions: add, sub, multi, div\nor q for quit')
     fcn = str(input())
 
-    if a or b or fcn != 'q':
+    if fcn != 'q':
         return a, b, fcn
 
+    else:
+        print('Bye')
+        quit()
 
-up = user_input()
 
-if up[2] == 'add':
-    print(cc.add(up[0], up[1]))
+def main_function():
 
-elif up[2] == 'sub':
-    print(cc.sub(up[0], up[1]))
+    up = user_input()
 
-elif up[2] == 'multi':
-    print(cc.multi(up[0], up[1]))
+    if up[2] == 'add':
+        print(cc.add(up[0], up[1]))
 
-elif up[2] == 'div':
-    print(cc.div(up[0], up[1]))
+    elif up[2] == 'sub':
+        print(cc.sub(up[0], up[1]))
 
-else:
-    print('Something went wrong!')
+    elif up[2] == 'multi':
+        print(cc.multi(up[0], up[1]))
+
+    elif up[2] == 'div':
+        print(cc.div(up[0], up[1]))
+
+    else:
+        print('Something went wrong!')
+
+
+main_function()
