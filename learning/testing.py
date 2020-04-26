fa4b8fd930f6688e029f18e11fcbb4899694a529
Roman <cypherpanda@pm.me>
2020-04-26 12:31:28 +0200

    learning things

--- learning/testing.py
+++ learning/testing.py
@@ -233,20 +233,23 @@
 # ----------------------------------------------
 
 
-def div(a, b):
-    print(a / b)
+# def div(a, b):
+#     print(a / b)
+#
+#
+# def smart_div(func):
+#
+#     def inner(a, b):
+#         if a < b:
+#             a, b = b, a
+#         return func(a, b)
+#
+#     return inner
+#
+#
+# div1 = smart_div(div)
+#
+# div1(2, 4)
 
+# ------------------------------------------------
 
-def smart_div(func):
-
-    def inner(a, b):
-        if a < b:
-            a, b = b, a
-        return func(a, b)
-    
-    return inner
-
-
-div1 = smart_div(div)
-
-div1(2, 4)
