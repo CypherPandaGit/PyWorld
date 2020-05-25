d8953d51bf85e4074a0bc80ac7a390cfb345698e
Roman <cypherpanda@pm.me>
2020-05-25 15:43:28 +0200

    learning things

--- learning/testing.py
+++ learning/testing.py
@@ -298,3 +298,14 @@ print(s2.average())
 
 # --------------------------------------------------
 
+class Student:
+
+    def __init__(self, name, rollno):
+        self.name = name
+        self.rollno = rollno
+
+
+s1 = Student('Jay', 2)
+s2 = Student('Jennifer', 1)
+
+s1.show()
