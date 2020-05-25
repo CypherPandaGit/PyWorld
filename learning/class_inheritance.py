0cbbbe8c3548dee58510353db3202877f3362f0f
Roman <cypherpanda@pm.me>
2020-05-25 16:30:49 +0200

    class inheritance

--- learning/class_inheritance.py
+++ learning/class_inheritance.py
@@ -7,17 +7,26 @@ class A:
         print('Feature 2 working too.')
 
 
+#class B():
 class B(A):
 
     def feature3(self):
-        print('We have also feature #3.')
+        print('We have also feature #3 in class B.')
 
     def feature4(self):
-        print('Feature 4? What else?')
+        print('Feature 4? What else? Class B FTW')
+
+
+#class C(A, B):
+class C(B):
+
+    def feature5(self):
+        print('Feature 5 from class C')
 
 
 a1 = A()
 b1 = B()
+c1 = C()
 
 a1.feature1()
 a1.feature2()
@@ -26,3 +35,5 @@ b1.feature1()
 b1.feature2()
 b1.feature3()
 b1.feature4()
+
+c1.feature5()
